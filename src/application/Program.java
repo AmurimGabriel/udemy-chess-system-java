ff17ec3daf012d85d4af8367c0031079c98fe349
Gabriel Amurim <amurim.souza27@gmail.com>
2020-06-03 21:06:04 -0300

    Starting to implement Board, Piece and Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello Word!");
+		
+		Board board = new Board(8, 8);
+
 	}
 }
