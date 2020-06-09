e63642c0a0cdc2f6a853b52b61c2232c855fa01c
Gabriel Amurim <amurim.souza27@gmail.com>
2020-06-09 19:15:16 -0300

    CHECKMATE logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,14 +56,22 @@ public class UI {
 		printBoard(chessMatch.getPieces());
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		System.out.println();
 
-		printCapturedPieces(captured);
-		System.out.println();
+		if (!chessMatch.getCheckMate()) {
 
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			System.out.println();
+
+			printCapturedPieces(captured);
+			System.out.println();
+
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("WINNER: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
